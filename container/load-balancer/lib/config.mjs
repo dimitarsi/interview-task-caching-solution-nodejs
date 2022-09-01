7e610928f93782cb64a74eba6c1d3033a1f7a6e1
Dimitar Iliev <dimitarsi@mail.bg>
2022-09-01 18:19:54 +0200

    chore: prepare docker env
    Setup a docker-compose environment running a fake
    load balancer instance with 3 nginx servers

---
+++ container/load-balancer/lib/config.mjs
@@ -0,0 +1,5 @@
+export const serverInstances = [
+    "http://deployment-01",
+    "http://deployment-02",
+    "http://deployment-03",
+]
---
+++ container/load-balancer/lib/server.mjs
@@ -0,0 +1,40 @@
+import express from 'express';
+import { serverInstances } from './config.mjs';
+import cookieParser from 'cookie-parser';
+import http from 'http'
+
+const app = express();
+
+const LB_AFFINITY = 'lb-affinity';
+
+app.use(cookieParser());
+
+app.get("*", async (req, res) => {
+    const affinity = parseInt(req.cookies?.[LB_AFFINITY] || "");
+    const index = isNaN(affinity) ? Math.floor(Math.random() * serverInstances.length) : affinity;
+    
+    const instance = serverInstances[index];
+    const searchParams = new URLSearchParams()
+    
+    for(const param in req.query) {
+        searchParams.set(param, req.query[param])
+    }
+
+    const url = new URL(`${req.path}?${searchParams.toString()}`, instance);
+    
+    res.setHeader(LB_AFFINITY, `${index} - ${serverInstances[index]}`)
+
+    // Short lived cookie - 5s, to enforce all follow-up requests stick with the same server
+    res.cookie(LB_AFFINITY, index, {expires: new Date(Date.now() + 500), httpOnly: true})
+
+    http.get(url, proxyReq => {
+        proxyReq.pipe(res)
+    })
+
+    console.log(`Proxy request ${req.path} ->`, url.toString(), `| Affintiy: ${affinity}`, ` | Actual: ${index}`);
+
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Load balancer listening on port ${process.env.PORT}`)
+})
