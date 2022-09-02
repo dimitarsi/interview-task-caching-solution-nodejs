ccc1d2402bf3697eb4e509f8465cab15b4b54acb
Dimitar Iliev <dimitarsi@mail.bg>
2022-09-02 15:47:07 +0200

    refactor: server a third nginx server with unique cache buster; cache server port listening

--- container/cache-server/lib/constants.mjs
+++ container/cache-server/lib/constants.mjs
@@ -3,5 +3,5 @@ export const CACHE_MISS = 2;
 
 export const CACHED_INSTANCE = 'http://load-balancer:8080'
 
-export const CACHE_PORT = 7777; // parseInt(process.env.PORT || "5555")
-export const CACHE_API_PORT = 8888; // CACHE_PORT + 1
+export const CACHE_PORT = parseInt(process.env.PORT || "7777")
+export const CACHE_API_PORT = parseInt(process.env.CACHE_API_PORT || "8888")
